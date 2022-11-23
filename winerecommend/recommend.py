7dac7c2db3e7887b6c050d1bb742e6df679614dc
chriss060 <alicevravo@gmail.com>
2022-11-23 16:44:33 +0900

    c

--- winerecommend/recommend.py
+++ winerecommend/recommend.py
@@ -5,9 +5,6 @@ client = MongoClient("3.38.57.203:27017")
 db = client['winedb']
 
 
-# 추천시스템
-
-
 def preprocess_doc():
     docs = db.wine_db.find({},{'_id':0, 'wine_id':1, 'sweet':1, 'acidic':1, 'body':1, 'tannic':1 ,'winetype_':1, 'kr_grape_':1})
     doc = pd.DataFrame(docs, columns=['wine_id','sweet','acidic','body','tannic','winetype_','kr_grape_'])
@@ -24,8 +21,8 @@ def find_similar_wine(input_vec, doc):
             dist[i]=np.sum(np.square(input_vec[0][1:] - temp.to_numpy()))
 
     dist = dict(sorted(dist.items(), key= lambda x:x[1])[:5])
-    #list(dist.keys())
-    return dist
+    
+    return list(dist.keys())
 
 def recommend_similar_wine(input_vec, doc):
     fields = {'_id':0, 'wine_id':1, 'kname':1, 'ename':1, 'winery':1, 'kr_country':1, 'kr_region':1, 'sweet':1, 'acidic':1, 'body':1, 'tannic':1 ,'winetype':1, 'kr_grape_list':1, 'note_list':1,'food_list':1 }
