c205bc3e3aae542ea5534e7a5778535bcfad49cf
chriss060 <alicevravo@gmail.com>
2022-11-28 10:13:51 +0900

    c

--- winerecommend/urls.py
+++ winerecommend/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('/similar_all/<int:wine_id>', views.SimilarWineAllListView().as_view()),
-    path('/similar_celler/<int:wine_id>', views.SimilarWineCellerListView.as_view())
+    path('/similar_all', views.SimilarWineAllListView().as_view()),
+    path('/similar_celler', views.SimilarWineCellerListView.as_view())
 
 ]
--- winerecommend/views.py
+++ winerecommend/views.py
@@ -3,11 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from wabawachi.settings import SECRET_KEY
 from .recommend import *
+from winesearch.models import Winesearch
 from wineceller.models import WineCeller
 import jwt
 
 class SimilarWineAllListView(APIView):
-        def get(self, request, wine_id):
+        def get(self, request):
+                
+                access = request.COOKIES['access']
+                payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])  
+                pk = payload.get('user_id') 
+                
+                wine_id = Winesearch.objects.filter(user_id=pk)[0].wine_id
     
                 doc = preprocess_doc()
                 input_vec = doc[doc['wine_id']==wine_id].values
@@ -16,12 +23,14 @@ class SimilarWineAllListView(APIView):
                 return Response(list(wine_list))
 
 class SimilarWineCellerListView(APIView):
-        def get(self, request, wine_id):
+        def get(self, request):
                 
                 #user
                 access = request.COOKIES['access']
                 payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])  
                 pk = payload.get('user_id') 
+                #wine_id
+                wine_id = Winesearch.objects.filter(user_id=pk)[0].wine_id
                 
                 if not WineCeller.objects.filter(owner_id=pk).exists():
                         raise ModuleNotFoundError('WINECELLER DOES NOT EXISTS')
